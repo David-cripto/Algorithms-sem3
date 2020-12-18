c1cbec48000b025cfbf44ea331be3edb86a2d0c4
David-cripto <li.d@phystech.edu>
2020-12-18 09:31:15 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,38 +3,38 @@
 #include <string>
 #include <algorithm>
 
-std::vector<int> prefix(const std::string& pattern, const std::string& str) {
+std::vector<int> findEntries(const std::string& pattern, const std::string& str) {
 	std::string s = pattern + "#" + str;
 	int n = static_cast<int>(s.length());//len of s
 	int p = static_cast<int>(pattern.length());
 	std::vector<int> pref(p);// prefix-fun
 	std::vector<int> positions;
-	for (int i = 1; i < p; ++i) {
-		int k = pref[i - 1];
+	int k;
+
+	for (int i = 1; i < n; ++i) {
+		if (i < p) {
+			k = pref[i - 1];
+		} else if (i == p) {
+			k = 0;
+		}
+
 		while (k > 0 && s[i] != s[k]) {//count k
 			k = pref[k - 1];
 		}
-		if (s[i] == s[k]) {
-			k++;
-		}
-		pref[i] = k;
-	}
 
-	int k = 0;
-
-	for (int i = p; i < n; ++i) {
-		while (k > 0 && s[i] != s[k]) {//count k
-			k = pref[k - 1];
-		}
 		if (s[i] == s[k]) {
 			k++;
 		}
 
-		if (k == p) {
+		if (i >= p && k == p) {
 			positions.push_back(i - p - k);
 			k = pref[k - 1];
 		}
+		if (i < p) {
+			pref[i] = k;
+		}
 	}
+
 	return positions;
 }
 
@@ -42,7 +42,7 @@ int main() {
 	std::string pattern;
 	std::string str;
 	std::cin >> pattern >> str;
-	std::vector<int> p = prefix(pattern, str);
+	std::vector<int> p = findEntries(pattern, str);
 	for (int i = 0; i < p.size(); ++i) {
 		std::cout << p[i] << " ";
 	}
