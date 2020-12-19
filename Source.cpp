77cd39c59bab249df7d05c372956f3f21f81f05a
David-cripto <li.d@phystech.edu>
2020-12-19 10:37:55 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <iomanip>
 
 using std::vector;
 
@@ -8,58 +9,66 @@ struct Point {
 	Point(double x = 0, double y = 0, double z = 0) :
 		x(x), y(y), z(z) {}
 
+	Point(const Point& other) :
+		x(other.x), y(other.y), z(other.z) {}
+
 	//координаты точки
 	double x;
 	double y;
 	double z;
+
+	Point& operator += (const Point& other);
+	Point& operator -= (const Point& other);
 };
+
+Point operator + (const Point& other1, const Point& other2);
+Point operator - (const Point& other1, const Point& other2);
+Point operator * (double number, const Point& other);
+Point operator * (const Point& other, double number);
 
 //квадрат расстояния между точками
 double dist(Point p1, Point p2) {
 	return (p1.x - p2.x)* (p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y) + (p1.z - p2.z)* (p1.z - p2.z);
 }
 
-double distance(const vector<Point>& pointers) {
+double ternarySearch(const Point& point1, const Point& point2, const Point& point3, const Point& point4) {
 	double eps = 0.00000000001;
-	Point v1(pointers[1].x - pointers[0].x, pointers[1].y - pointers[0].y, pointers[1].z - pointers[0].z);
-	Point v2(pointers[3].x - pointers[2].x, pointers[3].y - pointers[2].y, pointers[3].z - pointers[2].z);
-	Point p_1 = pointers[0];
-	Point p_2 = pointers[2];
-	Point np_1 = pointers[0];
-	Point np_2 = pointers[2];
-	Point p1;
-	Point p2;
-	vector<double> d;
-	d.push_back(dist(pointers[0], pointers[2]));
-	d.push_back(dist(pointers[0], pointers[3]));
-	d.push_back(dist(pointers[1], pointers[3]));
-	d.push_back(dist(pointers[1], pointers[2]));
+	Point first_segment(point2 - point1);
+	Point second_segment(point4 - point3);
+	Point fixed_point_in_first_segment(point1);
+	Point fixed_point_in_second_segment(point3);
+	Point new_change_point_for_fixed_point_in_first_segment(point1);
+	Point new_change_point_for_fixed_point_in_second_segment(point3);
+	Point change_point_in_first_segment;
+	Point change_point_in_second_segment;
+	vector<double> arr_of_distance;
+
+	arr_of_distance.push_back(dist(point1, point3));
+	arr_of_distance.push_back(dist(point1, point4));
+	arr_of_distance.push_back(dist(point2, point4));
+	arr_of_distance.push_back(dist(point2, point3));
 
 	for (int i = 0; i < 10000; ++i) {
 		double t_l = 0;
 		double t_r = 1;
-		p_1 = np_1;
-		p_2 = np_2;
+		fixed_point_in_first_segment = new_change_point_for_fixed_point_in_first_segment;
+		fixed_point_in_second_segment = new_change_point_for_fixed_point_in_second_segment;
 
 		while (t_r - t_l > eps) {
 			double m1 = t_l + (t_r - t_l) / 3;
 			double m2 = t_r - (t_r - t_l) / 3;
-			p1.x = pointers[0].x + m1 * v1.x;
-			p1.y = pointers[0].y + m1 * v1.y;
-			p1.z = pointers[0].z + m1 * v1.z;
 
-			p2.x = pointers[0].x + m2 * v1.x;
-			p2.y = pointers[0].y + m2 * v1.y;
-			p2.z = pointers[0].z + m2 * v1.z;
+			change_point_in_first_segment = point1 + m1 * first_segment;
+			change_point_in_second_segment = point1 + m2 * first_segment;
 
-			if (dist(p1, p_2) > dist(p2, p_2)) {
+			if (dist(change_point_in_first_segment, fixed_point_in_second_segment) > dist(change_point_in_second_segment, fixed_point_in_second_segment)) {
 				t_l = m1;
 			} else {	
 				t_r = m2;
 			}
 		}
 
-		np_1 = p1;
+		new_change_point_for_fixed_point_in_first_segment = change_point_in_first_segment;
 		double s_l = 0;
 		double s_r = 1;
 
@@ -67,46 +76,94 @@ double distance(const vector<Point>& pointers) {
 			double m1 = s_l + (s_r - s_l) / 3;
 			double m2 = s_r - (s_r - s_l) / 3;
 
-			p1.x = pointers[2].x + m1 * v2.x;
-			p1.y = pointers[2].y + m1 * v2.y;
-			p1.z = pointers[2].z + m1 * v2.z;
+			change_point_in_first_segment = point3 + m1 * second_segment;
+			change_point_in_second_segment = point3 + m2 * second_segment;
 
-			p2.x = pointers[2].x + m2 * v2.x;
-			p2.y = pointers[2].y + m2 * v2.y;
-			p2.z = pointers[2].z + m2 * v2.z;
-			if (dist(p1, p_1) > dist(p2, p_1)) {
+			if (dist(change_point_in_first_segment, fixed_point_in_first_segment) > dist(change_point_in_second_segment, fixed_point_in_first_segment)) {
 				s_l = m1;
 			} else {
 				s_r = m2;
 			}
 		}
 
-		np_2 = p1;
+		new_change_point_for_fixed_point_in_second_segment = change_point_in_first_segment;
 	}
 	
-	d.push_back(dist(p_1, p_2));
+	arr_of_distance.push_back(dist(fixed_point_in_first_segment, fixed_point_in_second_segment));
 
-	double ans = d[0];
+	double ans = arr_of_distance[0];
 
-	for (int i = 0; i < d.size(); ++i) {
-		if (ans > d[i]) {
-			ans = d[i];
+	for (int i = 0; i < arr_of_distance.size(); ++i) {
+		if (ans > arr_of_distance[i]) {
+			ans = arr_of_distance[i];
 		}
 	}
 	return sqrt(ans);
 }
 
-int main() {
-	double x, y, z;//координаты
-	vector<Point> pointers;
+std::istream& operator >> (std::istream& in, Point& temp) {
+	in >> temp.x >> temp.y >> temp.z;
+	return in;
+}
 
-	for (int i = 0; i < 4; ++i) {
-		std::cin >> x >> y >> z;
-		Point temp(x, y, z);
-		pointers.push_back(temp);
-	}
+int main() {
+	Point point1, point2, point3, point4;
 	
-	printf("%0.10f", distance(pointers));
-	
+	std::cin >> point1 >> point2 >> point3 >> point4;
+
+	std::cout.precision(10);
+	std::cout << std::fixed << std::setprecision(10) << ternarySearch(point1, point2, point3, point4);
+
 	return 0;
 }
+
+Point& Point::operator+=(const Point& other) {
+	this->x += other.x;
+	this->y += other.y;
+	this->z += other.z;
+
+	return *this;
+}
+
+Point& Point::operator-=(const Point& other) {
+	this->x -= other.x;
+	this->y -= other.y;
+	this->z -= other.z;
+
+	return *this;
+}
+
+Point operator-(const Point& other1, const Point& other2) {
+	Point temp(other1);
+
+	temp -= other2;
+
+	return temp;
+}
+
+Point operator*(double number, const Point& other) {
+	Point temp(other);
+	temp.x *= number;
+	temp.y *= number;
+	temp.z *= number;
+
+	return temp;
+}
+
+Point operator*(const Point& other, double number) {
+	Point temp(other);
+	temp.x *= number;
+	temp.y *= number;
+	temp.z *= number;
+
+	return temp;
+}
+
+Point operator+(const Point& other1, const Point& other2) {
+	Point temp(other1);
+
+	temp += other2;
+
+	return temp;
+}
+
